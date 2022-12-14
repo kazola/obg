09a40b98c9c3970fe410a64943e7aee3ff3d6456
kaz <kaz>
2022-12-14 13:16:19 -0500

    more

--- setup.py
+++ setup.py
@@ -1,10 +1,28 @@
-from setuptools import setup
+from setuptools import setup, find_packages
+
 
 setup(
     name='fleak',
+    version='0.8.412',
+    description='Web App to interact with Lowell Instruments loggers',
+    url='https://github.com/LowellInstruments/fleak',
+    author='Lowell Instruments',
+    author_email='joaquim@lowellinstruments.com',
     entry_points={
         'console_scripts': [
-            'fleak = fleak:main',
+            # exe_name=package.pyfilename:functionname
+            'fleak=fleak:main',
         ],
-    }
+    },
+    # next line is crucial
+    packages=find_packages(),
+    install_requires=[
+        'flet>=0.1.33',
+        'bleak',
+        'numpy',
+        'pandas',
+        'h5py',
+        'humanize',
+        'lowell-mat@git+https://github.com/lowellinstruments/lowell-mat.git@poor'
+    ]
 )

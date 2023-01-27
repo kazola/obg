8cc3437486ec2361419b0fb61d56cde52b77b285
kazola <kazola>
2023-01-27 16:22:00 -0500

    more

--- setup.py
+++ setup.py
@@ -1,12 +1,6 @@
 from setuptools import setup, find_packages
 
 
-# grab packages from requirements files
-with open('requirements311.txt') as f:
-    rr = f.readlines()
-
-
-
 setup(
     name='fleak',
     version='0.8.413',
@@ -21,6 +15,10 @@ setup(
         ],
     },
     packages=find_packages(),
-    # defined at top of this file
-    install_requires=rr
+    install_requires=[
+        'flet',
+        'bleak',
+        'humanize',
+        'numpy'
+    ]
 )

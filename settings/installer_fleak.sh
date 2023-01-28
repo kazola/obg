82769c5d93c338216dcf3217c24cd84da0d6b9cf
kaz <kaz>
2023-01-28 01:26:19 +0100

    more

--- settings/installer_fleak.sh
+++ settings/installer_fleak.sh
@@ -11,18 +11,18 @@ printf '> CURL installer for Lowell Instruments FLEAK GUI console\n'
 printf '> step 1) Creating virtual env'
 rm -rf venv_fleak
 python -m venv venv_fleak
-. venv_fleak/bin/activate
+source venv_fleak/bin/activate
 rv=$?
 if [ "$rv" -ne 0 ]; then
-    printf 'error creating fleak virtual environment'
+    printf 'error creating fleak virtual environment\n'
     exit 1
 fi
 
 
-printf '> step 2) Cloning repository fleak'
+printf '> step 2) Cloning repository fleak\n'
 pip install git+https://github.com/lowellinstruments/fleak.git
 rv=$?
 if [ "$rv" -ne 0 ]; then
-    printf 'error installing fleak'
+    printf 'error installing fleak\n'
     exit 1
 fi

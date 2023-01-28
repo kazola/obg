afedd51f5ebec94ba4e304d07439d010814a15c5
kaz <kaz>
2023-01-28 01:49:47 +0100

    more

--- installer/installer_fleak.sh
+++ installer/installer_fleak.sh
@@ -3,8 +3,17 @@ echo; echo; echo
 
 
 # -----------------------------------------------------------------------
-# curl -L https://raw.githubusercontent.com/LowellInstruments/fleak/master/settings/installer_fleak.sh | bash
+# curl -L https://raw.githubusercontent.com/LowellInstruments/fleak/master/installer/installer_fleak.sh | bash
 # -----------------------------------------------------------------------
+
+
+_e() {
+  rv=$1
+  if [ "$rv" -ne 0 ]; then
+    printf '%s' "$2"
+    exit 1
+fi
+}
 
 
 printf '> CURL installer for Lowell Instruments FLEAK GUI console\n'
@@ -12,24 +21,18 @@ printf '> step 1) Creating virtual env\n'
 rm -rf venv_fleak
 python3 -m venv venv_fleak
 source venv_fleak/bin/activate
-rv=$?
-if [ "$rv" -ne 0 ]; then
-    printf 'error creating fleak virtual environment\n'
-    exit 1
-fi
+_e $? 'error creating fleak virtual environment\n'
 
 
 printf '> step 2) Installing MAT library'
-
+git clone https://github.com/lowellinstruments/lowell-mat.git -b v4
+cd lowell-mat && export MY_IGNORE_REQUIREMENTS_TXT=1 && pip install . -v
+_e $? 'error installing MAT library\n'
 
 
 printf '> step 3) Cloning repository fleak\n'
 pip install git+https://github.com/lowellinstruments/fleak.git
-rv=$?
-if [ "$rv" -ne 0 ]; then
-    printf 'error installing fleak\n'
-    exit 1
-fi
+_e $? 'error installing fleak\n'
 
 
 printf '> done! now you type the following to run the fleak GUI'
